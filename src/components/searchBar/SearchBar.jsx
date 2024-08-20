42ee080c5cd7607058707ccaeca1f84d96a7ded9
Blank67 <hmnshshrm67@gmail.com>
2024-08-20 12:12:19 +0530

    search bar css

--- src/components/searchBar/SearchBar.jsx
+++ src/components/searchBar/SearchBar.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import PropTypes from "prop-types";
-import { Button } from "react-bootstrap";
+import { Button, Container, Form } from "react-bootstrap";
 
 const SearchBar = ({ onSearch }) => {
     const [query, setQuery] = useState("");
@@ -10,15 +10,15 @@ const SearchBar = ({ onSearch }) => {
     };
 
     return (
-        <div>
-            <input
+        <Container className="d-flex justify-content-center align-items-center gap-3 mt-5">
+            <Form.Control
                 type="text"
                 value={query}
                 onChange={(e) => setQuery(e.target.value)}
                 placeholder="Search for movies..."
             />
             <Button onClick={handleSearch}>Search</Button>
-        </div>
+        </Container>
     );
 };
 
